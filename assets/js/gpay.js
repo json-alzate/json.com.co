25673f700b2545c39eb5c0e866f996264cf8961b
json-alzate <developer.alzate@gmail.com>
2022-03-03 20:50:04 -0500

    🧐learning about gpay

---
+++ assets/js/gpay.js
@@ -0,0 +1,13 @@
+// define la version de la api
+const baseRequest = {
+    apiVersion: 2,
+    apiVersionMinor: 0
+};
+
+const tokenizationSpecification = {
+    type: 'PAYMENT_GATEWAY',
+    parameters: {
+        'gateway': 'example',
+        'gatewayMerchantId': 'exampleGatewayMerchantId'
+    }
+};
